c622887dc050423eaeefab1d203ffd63e87eb86d
Paulo Victor Ribeiro Prediger <142063372+pauloprediger@users.noreply.github.com>
2024-04-25 16:47:43 -0300

    Atualizações

---
+++ JogoAdivinhaNumero/scripts/h1.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Paulo Prediger'
+
+let content = document.querySelector('p');
+content.innerHTML = 'No ano de 2023 deu início sua segunda graduação na Estácio, na área de TI, no curso de Análise e Desenvolvimento de Sistemas, com o objetivo de, futuramente, se tornar um desenvolvedor fullstack. Para concluir esse objetivo tem uma rotina dividida entre estudos e trabalho, a fim de aperfeiçoar suas habilidades. Atualmente está em processo de mudança e espera conseguir conciliar suas duas paixões, gastronomia e tecnologia.'
--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,2 +1,9 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+function verificarChute() {
+    
+}
