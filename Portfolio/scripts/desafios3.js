e6c40a62284f6c236097ba11555ac328c7765510
Paulo Victor Ribeiro Prediger <142063372+pauloprediger@users.noreply.github.com>
2024-09-19 21:06:36 -0300

    Update desafio 3

--- Portfolio/scripts/desafios3.js
+++ Portfolio/scripts/desafios3.js
@@ -11,5 +11,26 @@ function calculaFatorial(numFat) {
     }
 }
 
+function converteDolar (reais) {
+    return (reais * 4.8).toFixed(2);
+}
+
+function areaSala (altura, largura){
+    let area = altura * largura;
+    let perimetro = 2 * (largura + altura);
+    return console.log(`O perimetro da sala é: ${perimetro}m e a área é ${area}m`);
+}
+
+function tabuadaRecursiva (num, multiplicador = 1){
+    if (multiplicador > 10){
+        return;
+    }else{
+        console.log(`${num} * ${multiplicador} = ${num * multiplicador}`);
+        tabuadaRecursiva (num, multiplicador + 1);
+    }
+}
 console.log(calculaIMC(78, 1.75));
 console.log(calculaFatorial(5));
+console.log(converteDolar(9));
+areaSala(20, 50);
+tabuadaRecursiva(7);
